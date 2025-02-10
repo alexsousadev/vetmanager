1173713225e62d64563f85a8d38cbf71595f463b
alexsousadev <alexandrosah5776@gmail.com>
2025-02-10 00:13:02 -0300

    ajustes requisição

--- app.ts
+++ app.ts
@@ -51,9 +51,7 @@ const swaggerOptions = {
 const swaggerDocument = swaggerJSDoc(swaggerOptions);
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use(cors({
-    origin: "http://localhost:8080"
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(bodyParser.json());
